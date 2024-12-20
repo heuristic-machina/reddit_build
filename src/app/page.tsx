17df7b8d984bf3ca0d98ba37e085f99a8725243a
heuristic_machina <machine_learner@yahoo.com>
2024-12-19 18:12:38 -0700

    file time reverse from og github repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,3 @@
-import { Button } from '@nextui-org/react';
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
-
-
-export default async function Home() {
-  const session = await auth();
-
-
-  return (
-    <div>
-      <form action={actions.signIn}>
-        <Button className='mb-2' type='submit'>
-          Sign In
-        </Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type='submit'>
-          Sign Out
-        </Button>
-      </form>
-
-      {session?.user ? (
-        <div className='text-green-600'>Signed In: {JSON.stringify(session.user)}</div>
-      ) : (
-        <div className='text-red-600'>Signed Out</div>
-      )}
-        <Profile />
-    </div>
-  );
-}
+export default function Home() {
+  return <div className='text-white'>Home Page</div>
+  }
