5b1a740bc4b48a3ef79f65900ef8ec168749eea7
heuristic_machina <machine_learner@yahoo.com>
2024-12-19 23:20:05 -0700

    server action createTopic wired up to client component

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,4 +1,8 @@
 'use server'
 
-export async function createTopic() {}
+export async function createTopic(formData: FormData) {
+    const name = formData.get('topic');
+    const description = formData.get('description');
+    console.log(name, description);
+}
 //revalidate homepage
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -18,8 +18,18 @@ export default function TopicCreateForm() {
                 <form action={actions.createTopic}>
                     <div className='flex flex-col gap-4 p-4 w-80'>
                         <h3 className='text-lg'>Create a Topic</h3>
-                        <Input label='Topic' labelPlacement='outside' placeholder='Topic' />
-                        <Textarea label='Description' labelPlacement='outside' placeholder='Describe your topic' />
+                        <Input 
+                        name='topic' 
+                        label='Topic' 
+                        labelPlacement='outside' 
+                        placeholder='Topic' 
+                        />
+                        <Textarea 
+                        name='description'
+                        label='Description' 
+                        labelPlacement='outside' 
+                        placeholder='Describe your topic' 
+                        />
                         <Button type='submit'>Submit</Button>
                     </div>
                 </form>
